aa6cdaf8880477bfd40887c9aae462dcca65b5e4
jeanfrancoisuccelli <jeanfrancoisuccelli@gmail.com>
2020-07-30 11:53:53 +0200

    correct error

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 
-const API_URL = "http://https://wildcircus2back.herokuapp.com/api/auth/";
+const API_URL = "http://wildcircus2back.herokuapp.com/api/auth/";
 
 class AuthService {
   login(username, password) {
     return axios
       .post(API_URL + "signin", {
         username,
-        password
+        password,
       })
-      .then(response => {
+      .then((response) => {
         if (response.data.accessToken) {
           localStorage.setItem("user", JSON.stringify(response.data));
         }
@@ -26,12 +26,12 @@ class AuthService {
     return axios.post(API_URL + "signup", {
       username,
       email,
-      password
+      password,
     });
   }
 
   getCurrentUser() {
-    return JSON.parse(localStorage.getItem('user'));;
+    return JSON.parse(localStorage.getItem("user"));
   }
 }
 
