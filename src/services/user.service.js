751410b735ea3758658d7f0e88d033be94a337ae
jf uccelli <jf.uccelli@telaqua.com>
2020-11-26 12:39:18 +0100

    added crossdomain: true

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -9,15 +9,15 @@ class UserService {
   }
 
   getUserBoard() {
-    return axios.get(API_URL + 'user', { headers: authHeader() });
+    return axios.get(API_URL + 'user', { headers: authHeader(), crossdomain: true } );
   }
 
   getModeratorBoard() {
-    return axios.get(API_URL + 'mod', { headers: authHeader() });
+    return axios.get(API_URL + 'mod', { headers: authHeader(), crossdomain: true });
   }
 
   getAdminBoard() {
-    return axios.get(API_URL + 'admin', { headers: authHeader() });
+    return axios.get(API_URL + 'admin', { headers: authHeader(), crossdomain: true });
   }
 }
 
